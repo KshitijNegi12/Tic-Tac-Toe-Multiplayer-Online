766a510e3552aa38b6b7403f4dc0bfc3da199a69
KshitijNegi12 <23kshitij.negi@gmail.com>
2024-08-23 17:45:49 +0530

    changed type and added packacge-lock

--- server.js
+++ server.js
@@ -1,51 +1,59 @@
-const http = require('http');
-const WebSocket = require('websocket').server;
+import express from 'express';
+import http from 'http';
+import { WebSocketServer } from 'ws';  
+
+const app = express();
+const port = 8080;
 const games = {};
 const clients = {};
 const CROSS_SYMBOL = 'x';
 const CIRCLE_SYMBOL = 'o';
 const WIN_STATES = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-  [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
   [0, 4, 8], [2, 4, 6]
 ];
 
-const httpServer = http.createServer((request, response) => {
-  //
-});
+const httpServer = http.createServer(app);
+const socketServer = new WebSocketServer({ server: httpServer });
 
-const socketServer = new WebSocket({ 'httpServer': httpServer });
-socketServer.on('request', request => {
-  const connection = request.accept(null, request.origin);
-  const clientId = Math.round(Math.random() * 100) + Math.round(Math.random() * 100) + Math.round(Math.random() * 100);
+socketServer.on('connection', (connection) => {
+  const clientId = generateClientId();
   clients[clientId] = { 'clientId': clientId, 'connection': connection };
 
-  connection.on('open', connectionOpened);
-  connection.on('close', () => {
-    for (const gameId in games) {
-      if (games[gameId].players[0].clientId === clientId) {
-        delete games[gameId];
-        sendAvailableGames();
-        break;
-      }
-    }
-    delete clients[clientId];
-  });
-  connection.on('message', messageHandler);
-
-  connection.send(JSON.stringify({ 'method': 'connect', 'clientId': clients[clientId].clientId }));
+  connection.send(JSON.stringify({ 'method': 'connect', 'clientId': clientId }));
   sendAvailableGames();
+
+  connection.on('message', (message) => messageHandler(message, clientId));
+
+  connection.on('close', () => handleDisconnection(clientId));
+
+  connection.on('error', (err) => console.error('WebSocket error:', err));
 });
 
-httpServer.listen(8080, () => { console.log('server listening on port 8080') });
+httpServer.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
-function connectionOpened() {
-  connection.send('connection with server opened');
+function generateClientId() {
+  return Math.round(Math.random() * 100) + Math.round(Math.random() * 100) + Math.round(Math.random() * 100);
 }
 
-function messageHandler(message) {
-  const msg = JSON.parse(message.utf8Data);
+function handleDisconnection(clientId) {
+  for (const gameId in games) {
+    if (games[gameId].players[0].clientId === clientId) {
+      delete games[gameId];
+      sendAvailableGames();
+      break;
+    }
+  }
+  delete clients[clientId];
+}
+
+function messageHandler(message, clientId) {
+  const msg = JSON.parse(message.toString());
   let player = {};
+
   switch (msg.method) {
     case 'create':
       player = {
@@ -55,21 +63,18 @@ function messageHandler(message) {
         'wins': 0,
         'lost': 0
       };
-      const gameId = Math.round(Math.random() * 100) + Math.round(Math.random() * 100) + Math.round(Math.random() * 100);
+      const gameId = generateClientId();
       const board = ['', '', '', '', '', '', '', '', ''];
       games[gameId] = {
         'gameId': gameId,
         'players': [player],
         'board': board
       };
-      const createPayLoad = {
-        'method': 'create',
-        'game': games[gameId]
-      };
-      const conn = clients[msg.clientId].connection;
-      conn.send(JSON.stringify(createPayLoad));
+      const createPayLoad = { 'method': 'create', 'game': games[gameId] };
+      clients[msg.clientId].connection.send(JSON.stringify(createPayLoad));
       sendAvailableGames();
       break;
+
     case 'join':
       player = {
         'clientId': msg.clientId,
@@ -79,18 +84,16 @@ function messageHandler(message) {
         'lost': 0
       };
       games[msg.gameId].players.push(player);
-
       clients[msg.clientId].connection.send(JSON.stringify({
         'method': 'join',
         'game': games[msg.gameId]
       }));
-
       makeMove(games[msg.gameId]);
-      sendAvailableGames(); // Add this line to refresh the available games list
+      sendAvailableGames();
       break;
+
     case 'makeMove':
       games[msg.game.gameId].board = msg.game.board;
-
       let currPlayer;
       let playerSymbol;
       msg.game.players.forEach((player) => {
@@ -99,47 +102,58 @@ function messageHandler(message) {
           playerSymbol = player.symbol;
         }
       });
-      let isWinner = false;
-      isWinner = WIN_STATES.some((row) => {
-        return row.every((cell) => games[msg.game.gameId].board[cell] === playerSymbol);
-      });
-      if (isWinner) {
-        const winPayLoad = {
-          'method': 'gameEnds',
-          'winner': playerSymbol
-        };
-        games[msg.game.gameId].players.forEach(player => {
-          if (clients[player.clientId]) {
-            clients[player.clientId].connection.send(JSON.stringify(winPayLoad));
-          }
-        });
-        setTimeout(() => {
-          refreshGame(msg.game.gameId);
-        }, 3000); // Ensure this delay is enough for message to be processed
-        break;
+
+      if (checkWinCondition(playerSymbol, msg.game.gameId)) {
+        declareWinner(playerSymbol, msg.game.gameId);
+      } else if (checkDrawCondition(msg.game.gameId)) {
+        declareDraw(msg.game.gameId);
       } else {
-        const isDraw = games[msg.game.gameId].board.every(cell => cell === 'x' || cell === 'o');
-        if (isDraw) {
-          const drawPayLoad = {
-            'method': 'draw'
-          };
-          games[msg.game.gameId].players.forEach(player => {
-            if (clients[player.clientId]) {
-              clients[player.clientId].connection.send(JSON.stringify(drawPayLoad));
-            }
-          });
-          setTimeout(() => {
-            refreshGame(msg.game.gameId);
-          }, 3000); // Ensure this delay is enough for message to be processed
-          break;
-        }
+        switchTurns(msg.game.gameId);
+        makeMove(games[msg.game.gameId]);
       }
-      games[msg.game.gameId].players.forEach((player) => {
-        player.isTurn = !player.isTurn;
-      });
-      makeMove(games[msg.game.gameId]);
       break;
   }
+}
+
+function checkWinCondition(symbol, gameId) {
+  return WIN_STATES.some((row) => row.every((cell) => games[gameId].board[cell] === symbol));
+}
+
+function checkDrawCondition(gameId) {
+  return games[gameId].board.every(cell => cell === 'x' || cell === 'o');
+}
+
+function declareWinner(symbol, gameId) {
+  const winPayLoad = { 'method': 'gameEnds', 'winner': symbol };
+  games[gameId].players.forEach(player => {
+    if (clients[player.clientId]) {
+      clients[player.clientId].connection.send(JSON.stringify(winPayLoad));
+    }
+  });
+  setTimeout(() => refreshGame(gameId), 3000);
+}
+
+function declareDraw(gameId) {
+  const drawPayLoad = { 'method': 'draw' };
+  games[gameId].players.forEach(player => {
+    if (clients[player.clientId]) {
+      clients[player.clientId].connection.send(JSON.stringify(drawPayLoad));
+    }
+  });
+  setTimeout(() => refreshGame(gameId), 3000);
+}
+
+function switchTurns(gameId) {
+  games[gameId].players.forEach(player => player.isTurn = !player.isTurn);
+}
+
+function makeMove(game) {
+  const payLoad = { 'method': 'updateBoard', 'game': game };
+  game.players.forEach((player) => {
+    if (clients[player.clientId]) {
+      clients[player.clientId].connection.send(JSON.stringify(payLoad));
+    }
+  });
 }
 
 function refreshGame(gameId) {
@@ -150,20 +164,6 @@ function refreshGame(gameId) {
       }
     });
   }
-}
-
-function makeMove(game) {
-  const payLoad = {
-    'method': 'updateBoard',
-    'game': game
-  };
-  game.board.forEach(cell => console.log(`  ${cell}`));
-  game.players.forEach((player) => {
-    console.log(`player ${player.clientId}`);
-    if (clients[player.clientId]) {
-      clients[player.clientId].connection.send(JSON.stringify(payLoad));
-    }
-  });
 }
 
 function sendAvailableGames() {
